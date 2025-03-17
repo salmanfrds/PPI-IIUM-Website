c68341d500f6493af175c3532efdf0e99e6fd917
salmanfrds <salmanfird4@outlook.com>
2025-03-18 03:50:53 +0800

    enable analytics

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import router from './router'; // ✅ Import the router
+import { inject } from '@vercel/analytics';
+
+inject()
 
 const app = createApp(App);
-
 app.use(router); // ✅ Register Vue Router
 app.mount('#app');
