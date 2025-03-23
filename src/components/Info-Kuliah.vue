d9aef6e610101f39c3cd4f469025ef42f9df3f35
salmanfrds <salmanfird4@outlook.com>
2025-03-24 05:26:22 +0800

    correct info kuiah

--- src/components/Info-Kuliah.vue
+++ src/components/Info-Kuliah.vue
@@ -22,13 +22,13 @@ export default {
 <template>
   <section class="flex justify-center items-center min-h-screen bg-gray-100 py-8 mt-16">
     <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-      <div v-for="faculty in faculties" :key="faculty.name" class="bg-white rounded-md shadow-md overflow-hidden p-4 flex flex-col items-center w-[20vw]">
+      <div v-for="faculty in faculties" :key="faculty.name" class="bg-white rounded-md shadow-md overflow-hidden p-4 flex flex-col items-center w-[40vw] md:w-[20vw]">
         <!-- Faculty Image -->
         <div class="w-full aspect-[4/3] bg-cover bg-center rounded-lg"
              :style="{ backgroundImage: `url(${faculty.image})` }">
         </div>
         <div class="w-full flex flex-col justify-between h-full">
-            <h3 class="text-base font-semibold text-gray-800">{{ faculty.name }}</h3>
+            <h3 class="text-base font-semibold text-gray-800 md:p-2">{{ faculty.name }}</h3>
             <!-- "See More" Button -->
             <a :href="faculty.link" 
                class="text-sm text-gray-600 px-4 py-2 transition mx-auto mt-4 border-t border-gray-900">
