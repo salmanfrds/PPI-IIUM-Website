125f0da97667d127b02ce80262f587ed45cd1430
salmanfrds <salmanfird4@outlook.com>
2025-04-12 20:10:14 +0800

    benerin header

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -94,11 +94,11 @@
 
     <!-- Mobile Menu Button -->
     <button 
-      class="xl:hidden text-zinc-100 p-2 rounded hover:bg-zinc-800/50 transition-colors"
+      class="xl:hidden text-zinc-100 p-1 rounded hover:bg-zinc-800/50 transition-colors"
       @click="$emit('toggleSidebar')"
       aria-label="Toggle mobile menu"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
       </svg>
     </button>
