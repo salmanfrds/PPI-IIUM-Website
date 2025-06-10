b192f0d07abfdec3df871166bd7f1139fd5734c0
salmanfrds <salmanfird4@outlook.com>
2025-06-10 14:53:27 +0800

    move kunjungan section

--- src/views/HomeLanding.vue
+++ src/views/HomeLanding.vue
@@ -1,11 +1,13 @@
 <script setup>
     import Hero from '@/components/Hero.vue';
+    import Kunjungan from '@/components/Kunjungan.vue';
     import Recent from '@/components/Recent.vue';
 </script>
 
 <template>
   <section>
     <Hero />
+    <Kunjungan />
     <Recent />
   </section>
 </template>
