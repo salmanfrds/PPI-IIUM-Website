78c94fb0ec1ba4ea14f0fcd862b7b1a43d07507a
salmanfrds <salmanfird4@outlook.com>
2025-08-12 09:04:51 +0800

    update theme

--- src/views/HomeLanding.vue
+++ src/views/HomeLanding.vue
@@ -4,12 +4,11 @@
     import Recent from '@/components/Recent.vue';
     import Merdeka from '../components/Merdeka.vue'
     import BannerMerdeka from '../components/Banner-Merdeka.vue';
-
 </script>
 
 <template>
   <section>
-    <Hero />
+    <Merdeka />
     <BannerMerdeka />
     <Kunjungan />
     <Recent />
