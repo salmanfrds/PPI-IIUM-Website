846d65da53fd62692c234e44b06011539ccfba01
salmanfrds <salmanfird4@outlook.com>
2025-09-23 10:33:29 +0800

    back to non-merdeka theme

--- src/views/HomeLanding.vue
+++ src/views/HomeLanding.vue
@@ -2,14 +2,14 @@
     import Hero from '@/components/Hero.vue';
     import Kunjungan from '@/components/Kunjungan.vue';
     import Recent from '@/components/Recent.vue';
-    import Merdeka from '../components/Merdeka.vue'
+    // import Merdeka from '../components/Merdeka.vue'
     import BannerMerdeka from '../components/Banner-Merdeka.vue';
 </script>
 
 <template>
   <section>
-    <Merdeka />
-    <BannerMerdeka />
+    <Hero />
+    <!-- <BannerMerdeka /> -->
     <Kunjungan />
     <Recent />
   </section>
